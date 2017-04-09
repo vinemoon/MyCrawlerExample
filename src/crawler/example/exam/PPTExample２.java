e0100894b81cdcc6609085cecff9a75cd69a8363
vinemoon <dallar18@yahoo.com.tw>
2017-04-09 12:05:29 +0800

    PPT作業2 查內文

--- src/crawler/example/exam/PPTExample２.java
+++ src/crawler/example/exam/PPTExample２.java
@@ -2,6 +2,12 @@ package crawler.example.exam;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
+import sun.text.normalizer.RangeValueIterator;
+
+
+
+import static sun.plugin.javascript.navig.JSType.Element;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -9,7 +15,7 @@ import org.apache.commons.logging.impl.SimpleLog;
  * @author Abola Lee
  *
  */
-public class PPTExample {
+public class PPTExample２ {
 	// commit test
 	// 123456
 	public static void main(String[] args) {
@@ -23,24 +29,41 @@ public class PPTExample {
 		// 遠端資料路徑//
 		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1491703259.A.E2A.html";
 		//String uri = "http://news.ltn.com.tw/news/life/paper/1092476";
-		System.out.println(
+
+		/*System.out.println(
 				CrawlerPack.start()
-				
+
 				// 參數設定
 			    .addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
-				
+
 				// 選擇資料格式 (三選一)
 				//.getFromJson(uri)
 			    .getFromHtml(uri)
 			    //.getFromXml(uri)
-			    
+
 			    // 這兒開始是 Jsoup Document 物件操作
 
-				.select("span.f3.push-content")
-				//.select("#main > div.content > h1")
+				.select("div#main-content.bbs-content.bbs-screen")
+				.text()
+		);	*/
 
 
-		);
+		Elements jsoup = CrawlerPack.start()
+
+				// 參數設定
+				.addCookie("over18", "1")    // 設定cookie
+				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+
+				// 選擇資料格式 (三選一)
+				//.getFromJson(uri)
+				.getFromHtml(uri)
+				//.getFromXml(uri)
+
+				// 這兒開始是 Jsoup Document 物件操作
+
+				.select("div#main-content.bbs-content.bbs-screen");
+		jsoup.select("div,span").remove();
+		System.out.println(jsoup.text());
 	}
 }
